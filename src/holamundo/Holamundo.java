d50dca27971e82b3d95a671cce57b1acc67bdda1
TORRES <TORRES@TORRES.mshome.net>
2021-07-14 11:20:35 -0600

    primer commit para repositorio remoto-

---
+++ src/holamundo/Holamundo.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundo;
+
+/**
+ *
+ * @author TORRES
+ */
+public class Holamundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+
+
+/**
+ *
+ * @author TORRES
+ */
+
+        System.out.print("hola mundo soy samir torres");
+    }
+    
+}
+    
+    
+
